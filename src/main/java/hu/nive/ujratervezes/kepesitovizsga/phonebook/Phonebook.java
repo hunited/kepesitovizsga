a308a47631b02ad09d711c5a29aae7956d9e9281
Sanyi <hunited@gmail.com>
2021-04-29 10:49:58 +0200

    Phonebook feladat első commit

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/phonebook/Phonebook.java
@@ -0,0 +1,28 @@
+package hu.nive.ujratervezes.kepesitovizsga.phonebook;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        if (output == null || output.isEmpty() || contacts == null) {
+            throw new IllegalArgumentException("Wrong Output");
+        }
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(output))) {
+            writeFile(contacts, writer);
+        } catch (IOException ioException) {
+            throw new IllegalArgumentException("Can not write file");
+        }
+    }
+
+    private void writeFile(Map<String, String> contacts, BufferedWriter writer) throws IOException {
+        for (Map.Entry row : contacts.entrySet()) {
+            writer.write(row.getKey() + ": " + row.getValue() + "\n");
+        }
+    }
+
+}
