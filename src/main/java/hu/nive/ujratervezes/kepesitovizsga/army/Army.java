13260da4810b21233c478b241dccf5ee7c8b1c17
Sanyi <hunited@gmail.com>
2021-04-29 11:54:04 +0200

    Army Sereg feladat első commit, hibás teszt később javítandó

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/army/Army.java
@@ -0,0 +1,36 @@
+package hu.nive.ujratervezes.kepesitovizsga.army;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Army {
+
+    private final List<MilitaryUnit> military = new ArrayList<>();
+
+    public void addUnit(MilitaryUnit militaryUnit) {
+        military.add(militaryUnit);
+    }
+
+    public void damageAll(int damage) {
+        System.out.println(military.size());
+        for (MilitaryUnit militaryUnit : military) {
+            militaryUnit.sufferDamage(damage);
+            System.out.println(militaryUnit.getHitPoints());
+        }
+        military.removeIf(militaryUnit -> militaryUnit.getHitPoints() > 25);
+    }
+
+    public int getArmyDamage() {
+        int result = 0;
+        for (MilitaryUnit militaryUnit : military) {
+            result += militaryUnit.doDamage();
+        }
+        return result;
+    }
+
+    public int getArmySize() {
+        return military.size();
+    }
+
+
+}
